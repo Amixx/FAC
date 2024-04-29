fd0e6e312a76cb835b3137951ea6f576b553ff1f
Imants Liepiņš <imants.liepins@whitedigital.eu>
2024-04-29 15:59:38 +0300

    fix M-SSR start script; implement (commented out) prerendering

--- m-ssr/nuxt.config.ts
+++ m-ssr/nuxt.config.ts
@@ -17,5 +17,6 @@ export default defineNuxtConfig({
   },
   nitro: {
     compressPublicAssets: true,
+    // prerender: { crawlLinks: true },
   },
 })
--- m-ssr/start.sh
+++ m-ssr/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-npm run build
-
 export NITRO_SSL_CERT="`cat ./certs/thesis-project.local.io.pem`"
 export NITRO_SSL_KEY="`cat ./certs/thesis-project.local.io-key.pem`"
 export PORT=8000
 export VITE_API_BASE_URL="https://thesis-project.local.io/api"
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
+npm run build
 exec node .output/server/index.mjs
