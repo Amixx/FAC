6ba3e55e33f3b3a3b4230561add0060ac0e449f9
Imants Liepiņš <imants.liepins@whitedigital.eu>
2024-05-01 20:26:51 +0300

    shopping platform: implement pagination in htmx

--- t-ssr/shopping-platform/src/Controller/MainPartialController.php
+++ t-ssr/shopping-platform/src/Controller/MainPartialController.php
@@ -2,11 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Order;
-use App\Repository\OrderItemRepository;
-use App\Repository\OrderRepository;
-use App\Repository\ProductCategoryRepository;
-use App\Repository\ProductRepository;
+use App\Service\DataService;
 use Htmxfony\Controller\HtmxControllerTrait;
 use Htmxfony\Response\HtmxResponse;
 use Htmxfony\Request\HtmxRequest;
@@ -18,90 +14,54 @@ class MainPartialController extends AbstractController
 {
     use HtmxControllerTrait;
 
-    private ProductRepository $productRepository;
-    private ProductCategoryRepository $productCategoryRepository;
-    private OrderRepository $orderRepository;
-    private OrderItemRepository $orderItemRepository;
+    private DataService $dataService;
 
-    public function __construct(
-        ProductRepository         $productRepository,
-        ProductCategoryRepository $productCategoryRepository,
-        OrderRepository           $orderRepository,
-        OrderItemRepository       $orderItemRepository
-    )
+    public function __construct(DataService $dataService)
     {
-        $this->productRepository = $productRepository;
-        $this->productCategoryRepository = $productCategoryRepository;
-        $this->orderRepository = $orderRepository;
-        $this->orderItemRepository = $orderItemRepository;
+        $this->dataService = $dataService;
     }
 
     #[Route('home-body', name: 'home_body')]
     public function homeBody(): HtmxResponse
     {
-        return $this->htmxRender('main/home-body.html.twig', [
-            'images' => [
-                'mob' => '/images/mob/home-hero.jpg',
-                'desk' => '/images/desk/home-hero.jpg',
-            ],
-            'newProducts' => $this->productRepository->findNewProducts(),
-            'popularProducts' => $this->productRepository->findPopularProducts(),
-            'productsWithDiscounts' => $this->productRepository->findProductsWithDiscounts(),
-        ]);
+        return $this->htmxRender('main/home-body.html.twig', $this->dataService->getHomeData());
     }
 
     #[Route('catalogue-body/{page}', name: 'catalogue_body')]
     public function catalogueBody(HtmxRequest $request, $page = 1): HtmxResponse
     {
-        $categories = $this->productCategoryRepository->findAll();
-
-        $selectedCategory = $request->get('category');
-        if ($selectedCategory) {
-            $products = $this->productRepository->findProductsByCategory($selectedCategory, $page);
-        } else {
-            $products = $this->productRepository->findProducts($page);
-        }
+        $data = $this->dataService->getCatalogueData($request->get('category'), $page);
 
         return $this->htmxRender('main/catalogue-body.html.twig', [
-            'categories' => $categories,
-            'selectedCategory' => $selectedCategory,
-            'products' => $products,
+            ...$data,
+            'toNextPage' => $data['nextPageData'] ? $this->generateUrl('catalogue-products', $data['nextPageData']) : null,
+            'toPrevPage' => $data['prevPageData'] ? $this->generateUrl('catalogue-products', $data['prevPageData']) : null,
         ]);
+    }
+
+    #[Route('catalogue-products/{page}', name: 'catalogue-products')]
+    public function catalogueProducts(HtmxRequest $request, $page = 1): HtmxResponse
+    {
+        $data = $this->dataService->getCatalogueData($request->get('category'), $page);
+
+        return $this->htmxRender('main/catalogue-products.html.twig', $data);
     }
 
     #[Route('product-body/{id}', name: 'product_body')]
     public function productBody(int $id): HtmxResponse
     {
-        $product = $this->productRepository->find($id);
-
-        return $this->htmxRender('main/product-body.html.twig', [
-            'product' => $product,
-        ]);
+        return $this->htmxRender('main/product-body.html.twig', $this->dataService->getProductData($id));
     }
 
     #[Route('cart-body', name: 'cart_body')]
     public function cartBody(): HtmxResponse
     {
-        $order = $this->orderRepository->getCurrentOrder();
-        if (!$order) {
-            $order = $this->orderRepository->createNewOrder();
-        }
-
-        return $this->htmxRender('main/cart-body.html.twig', [
-            'order' => $order,
-        ]);
+        return $this->htmxRender('main/cart-body.html.twig', $this->dataService->getCartData());
     }
 
     #[Route('checkout-body', name: 'checkout_body')]
     public function checkoutBody(): HtmxResponse
     {
-        $order = $this->orderRepository->getCurrentOrder();
-        if (!$order) {
-            return $this->redirectToRoute('home');
-        }
-
-        return $this->htmxRender('main/checkout-body.html.twig', [
-            'order' => $order,
-        ]);
+        return $this->htmxRender('main/checkout-body.html.twig', $this->dataService->getCheckoutData());
     }
 }
