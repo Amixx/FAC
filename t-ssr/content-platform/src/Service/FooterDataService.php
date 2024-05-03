448cf56af5d1ef1a86267e1eaf86d2c5f2a9dbb9
Imants Liepiņš <imants.liepins@whitedigital.eu>
2024-05-03 17:22:41 +0300

    recreate deleted file

---
+++ t-ssr/content-platform/src/Service/FooterDataService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+
+class FooterDataService
+{
+    private array $data;
+
+    public function __construct()
+    {
+        $this->data = JsonFileReaderService::readAndParseJson(__DIR__ . '/../../data/data.json');
+    }
+
+    public function getData(): array
+    {
+        return $this->data['footer'];
+    }
+}
